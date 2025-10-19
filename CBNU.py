5cd7c15a0a55926fb8f6b832240e3219fdb8c451
Kim Jinseong <154069891+CBNU1214@users.noreply.github.com>
2025-10-19 18:33:47 +0900

    Add files via upload

---
+++ CBNU.py
@@ -0,0 +1,9 @@
+# A list of numbers
+my_numbers = [10, 27, 8, 42, 15, 3]
+
+# Use the max() function to find the largest number
+largest_number = max(my_numbers)
+
+# Print the result
+print(f"The list of numbers is: {my_numbers}")
+print(f"The largest number in the list is: {largest_number}")
